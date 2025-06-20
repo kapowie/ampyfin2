3166db92b3f5071fe5897f329020b5b2613467ff
kapowie <jackperrett@gmail.com>
2025-06-20 13:30:37 +0100

    feat(config): add BigQuery env vars

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from os import environ as env
 
 from dotenv import load_dotenv
 
@@ -11,6 +10,9 @@ API_SECRET = os.getenv("API_SECRET")
 BASE_URL = os.getenv("BASE_URL")
 WANDB_API_KEY = os.getenv("WANDB_API_KEY")
 MONGO_URL = os.getenv("MONGO_URL")
+BQ_DATASET = os.getenv("BQ_DATASET")
+BQ_TABLE = os.getenv("BQ_TABLE")
+BQ_SERVICE_ACCOUNT = os.getenv("BQ_SERVICE_ACCOUNT")
 
 # Check and fail explicitly if something is missing
 required_vars = {
@@ -19,6 +21,8 @@ required_vars = {
     "BASE_URL": BASE_URL,
     "WANDB_API_KEY": WANDB_API_KEY,
     "MONGO_URL": MONGO_URL,
+    "BQ_DATASET": BQ_DATASET,
+    "BQ_TABLE": BQ_TABLE,
 }
 
 missing = [k for k, v in required_vars.items() if not v]
